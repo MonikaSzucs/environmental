ff8e1c3242b7ca595a17454e2b7800536fe8ae51
Monika Szucs <monika.szucs.work@gmail.com>
2024-04-07 22:39:51 -0700

    changed ?

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ export default function Home() {
     <>
       <main className={`${styles.main}`}>
         {
-          data?.map((i, index) => {
+          data.map((i, index) => {
             return(
               <div key={index}>
                 {i.author}
